5c101ff56a0804f81656e459b111bda89287561d
Nikola Badjevic <Ronin95@users.noreply.github.com>
2022-08-28 23:51:51 +0200

    chicken moved - chicken are moving at different speeds

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -2,6 +2,8 @@ class Clouds extends MovableObject {
     y = 20;
     width = 300;
     height = 200;
+    speed = 0.15;
+
     constructor() {
         super().loadImage('img/5_background/layers/4_clouds/1.png');
         this.x = Math.random()*500; // Number between 200 and 700
@@ -9,6 +11,10 @@ class Clouds extends MovableObject {
     }
 
     animate() {
+        this.moveLeft();
+    }
+    
+    moveLeft() {
         setInterval( () => {
             this.x -= 0.15;
         }, 1000/60)
