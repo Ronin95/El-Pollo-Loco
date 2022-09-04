cf469eedad2f76a03c3e0f78228de3c715fb1613
Nikola Badjevic <Ronin95@users.noreply.github.com>
2022-09-04 22:00:12 +0200

    gravity implemented

--- models/character.class.js
+++ models/character.class.js
@@ -1,7 +1,7 @@
 class Character extends MovableObject {
     height = 280;
     width = 150;
-    y = 190;
+    y = 80;
     x = 40;
     speed = 10;
     IMAGES_WALKING = [
@@ -18,7 +18,7 @@ class Character extends MovableObject {
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
         this.loadImages(this.IMAGES_WALKING);
-
+        this.applyGravity();
         this.animate();
     }
 
