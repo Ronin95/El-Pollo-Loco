9791baf385f8e48b4a669dc3519cf1236430204a
Nikola Badjevic <Ronin95@users.noreply.github.com>
2022-09-08 17:36:02 +0200

    pepe got hurt

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -63,6 +63,17 @@ class MovableObject {
                 this.y < mo.y + mo.height;
     }
 
+    hit() {
+        this.energy -= 5;
+        if (this.energy < 0) {
+            this.energy = 0;
+        }
+    }
+
+    isDead() {
+        return this.energy == 0;
+    }
+
     playAnimation(images) {
         let i = this.currentImage % images.length;
         let path = images[i];
