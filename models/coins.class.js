724fa4161e847a5b09189cd39dae4feb71ff9af1
Nikola Badjevic <Ronin95@users.noreply.github.com>
2022-09-12 22:40:41 +0200

    soon the game should have 3 levels in all

---
+++ models/coins.class.js
@@ -0,0 +1,12 @@
+class Coin {
+    height = 120;
+    width = 120;
+    y = 300;
+    x = 300;
+
+    constructor() {
+        super().loadImage('img/8_coin/coin_1.png');
+        this.x = Math.random() * 2700 + 500;
+        this.y = Math.random() * 300 + 50;
+    }
+}
