45ec4788902e022eee72f73450e4ab3d1b504e0c
Nikola Badjevic <Ronin95@users.noreply.github.com>
2022-09-20 15:55:35 +0200

    added some comments

--- models/keyboard.class.js
+++ models/keyboard.class.js
@@ -1,7 +1,7 @@
 class Keyboard {
-    LEFT = false;  // Left arrow
-    RIGHT = false; // Right arrow
-    SPACE = false; // Space bar
+    LEFT = false;  // Left arrow - Going left
+    RIGHT = false; // Right arrow - Going right
+    SPACE = false; // Space bar - Jump
     B = false;     // B letter for Bottle
     S = false;     // S letter for hidden Shuriken (Weapon)
 }
