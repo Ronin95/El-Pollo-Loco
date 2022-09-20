8a2f23e44a3613a4eb074688118c8075b5fef5b3
Nikola Badjevic <Ronin95@users.noreply.github.com>
2022-09-20 16:46:49 +0200

    Chicken class finished

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -2,33 +2,39 @@ class Chicken extends MovableObject {
     height = 70;
     width = 70;
     y = 390;
-    IMAGES_WALKING = [
+    CHICKEN_WALKING = [
         'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
         'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
         'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
     ];
-    chicken_walking_audio = new Audio('./audio/audio_chicken.mp3');
+
+    CHICKEN_DEAD = [
+        'img/3_enemies_chicken/chicken_small/2_dead/dead.png'
+    ];
+    // chickens_1_sound = new Audio('audio/audio_chicken_1.mp3');
+    // chickens_2_sound = new Audio('audio/audio_chicken_2.mp3');
 
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
-        this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.CHICKEN_WALKING);
+        this.loadImages(this.CHICKEN_DEAD);
         this.x = 300 + Math.random()*3000; // Number between 200 and 25  00
         this.speed = 0.35 + Math.random() * 0.4;
         this.animate();
     }
 
     animate() {
+        this.moveLeft();
         setInterval(() => {
-            this.chicken_walking_audio.play(); // chicken sound
-            this.chicken_walking_audio.volume = 0.05;
-            setInterval(() => {
-                this.moveLeft();
-            }, 1000 / 60);
-    
-            setInterval(() => {
-                this.playAnimation(this.IMAGES_WALKING);
-            }, 500);
-        }, 4000);
+            if(this.energy <= 0) {
+                this.energy = 0;
+                this.speed = 0;
+                this.loadImage('img/3_enemies_chicken/chicken_small/2_dead/dead.png');
+                this.playAnimation(this.CHICKEN_DEAD);
+            } else {
+                this.playAnimation(this.CHICKEN_WALKING);
+            }
+        }, 100);
     }
 
     
