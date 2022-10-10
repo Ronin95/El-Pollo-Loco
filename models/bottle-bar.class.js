b357e2eede95e6e546d3876f6c23829ebbb9761d
Nikola Badjevic <Ronin95@users.noreply.github.com>
2022-10-10 23:52:53 +0200

    removed obsolete code

--- models/bottle-bar.class.js
+++ models/bottle-bar.class.js
@@ -1,69 +1,10 @@
 class BottleBar extends DrawableObject {
-//     IMAGES_BOTTLES = [
-//         'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/0.png',
-//         'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/20.png',
-//         'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/40.png',
-//         'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/60.png',
-//         'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/80.png',
-//         'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/100.png'
-//      ];
-        bottles = 0;
-//      .rotate(45*Math.PI / 180)
-//      bottleImg = 'img/6_salsa_bottle/salsa_bottle.png';
-//      loadImage('img/6_salsa_bottle/salsa_bottle.png');
-
-    
 
     constructor() {
         super().loadImage('img/10_otherTools/bottle_amount.png');
-        // this.loadImages(this.IMAGES_BOTTLES);
-        // this.setBottles(0);
         this.x = 340;
         this.y = 13;
         this.width = 40;
         this.height = 50;
     }
-
-    /**
-     * Setting the number of bottles.
-     * 
-     * @method
-     * @name setBottles
-     * @kind method
-     * @memberof BottleBar
-     * @param {any} bottles
-     * @returns {void}
-     */
-//     setBottles(bottles) {
-//         this.bottles = bottles; // -> 0...5 
-//         let path = this.IMAGES_BOTTLES[this.resolveImageIndex()];
-//         this.img = this.imageCache[path];
-//     }
-
-    /**
-     * A method that returns an index for the image to be used.
-     * 
-     * @method
-     * @name resolveImageIndex
-     * @kind method
-     * @memberof BottleBar
-     * @returns {0 | 1 | 2 | 3 | 4 | 5}
-     */
-//     resolveImageIndex() {
-//         if(this.bottles <= 0) {
-//             return 0;
-//     }   else if(this.bottles > 0 && this.bottles <= 2) {
-//             return 1;
-//     }   else if(this.bottles > 2 && this.bottles <= 4) {
-//             return 2;
-//     }   else if(this.bottles > 4 && this.bottles <= 6) {
-//             return 3;
-//     }   else if(this.bottles > 6 && this.bottles <= 7) {
-//             return 4;
-//     }   else if(this.bottles > 7) {
-//             return 5;
-//     }   else {
-//             return 0;
-//     }
-//     }
 }
--- models/coin-bar.class.js
+++ models/coin-bar.class.js
@@ -1,68 +1,10 @@
 class CoinBar extends DrawableObject {
-//     IMAGES_COINS = [
-//         'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/0.png',
-//         'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/20.png',
-//         'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/40.png',
-//         'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/60.png',
-//         'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/80.png',
-//         'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/100.png'
-//     ];
-    coins = 0;
 
     constructor() {
         super().loadImage('img/8_coin/coin_1.png');
-        // this.loadImages(this.IMAGES_COINS);
-        // this.setCoins(0);
         this.x = 180;
         this.y = -30;
         this.width = 140;
         this.height = 140;
     }
-
-    
-
-    /**
-     * Setting the coins to the value of coins.
-     * 
-     * @method
-     * @name setCoins
-     * @kind method
-     * @memberof CoinBar
-     * @param {any} coins
-     * @returns {void}
-     */
-//     setCoins(coins) {
-//         this.coins = coins; // -> 0...5 
-//         let path = this.IMAGES_COINS[this.resolveImageIndex()];
-//         this.img = this.imageCache[path];
-//     }
-
-    /**
-     * A method that returns a number between 0 and 5.
-     * 
-     * @method
-     * @name resolveImageIndex
-     * @kind method
-     * @memberof CoinBar
-     * @returns {0 | 1 | 2 | 3 | 4 | 5}
-     */
-//     resolveImageIndex() {
-//         if (this.coins > 10) {
-//                 return 5;
-//         } else if (this.coins > 8 && this.coins <= 10) {
-//                 return 4;
-//         } else if (this.coins > 6 && this.coins <= 8) {
-//                 return 3;
-//         } else if (this.coins > 4 && this.coins <= 6) {
-//                 return 2;
-//         } else if (this.coins >= 2 && this.coins <= 4) {
-//                 return 1;
-//         } else if (this.coins < 2) {
-//                 return 0;
-//         } else {
-//                 return 0;
-//         }
-//     }
-
-
 }
