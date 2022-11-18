0d05b274afca9f9caddd2608e7a3cd1856965d3f
Nikola Badjevic <Ronin95@users.noreply.github.com>
2022-11-18 16:42:45 +0100

    adding some extra comments

--- js/settings.js
+++ js/settings.js
@@ -1,5 +1,14 @@
 let music = new Audio('audio/music.mp3');
 
+/**
+ * A function that is called when the checkbox is checked or unchecked.
+ * The function disables on enables the music for the game
+ * 
+ * @function
+ * @name disableSound
+ * @kind function
+ * @returns {void}
+ */
 function disableSound() {
     let sounds = document.getElementById('chkSound');
     if (sounds.checked == true) {
